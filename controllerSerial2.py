319ea35c2699eb1622ce891926997f63983669bc
Nico-Mansilla <nicolas.mansilla@sansano.usm.cl>
2024-10-19 21:31:17 -0300

    modifcaciones

--- controllerSerial2.py
+++ controllerSerial2.py
@@ -70,92 +70,7 @@ def formar_trozo(id_,dribb,kick, velocidad_x, velocidad_y, velocidad_th):
 
 
 
-class XboxController(object):
-    MAX_TRIG_VAL = math.pow(2, 8)
-    MAX_JOY_VAL = math.pow(2, 15)
-
-    def __init__(self):
-
-        self.LeftJoystickY = 0
-        self.LeftJoystickX = 0
-        self.RightJoystickY = 0
-        self.RightJoystickX = 0
-        self.LeftTrigger = 0
-        self.RightTrigger = 0
-        self.LeftBumper = 0
-        self.RightBumper = 0
-        self.A = 0
-        self.X = 0
-        self.Y = 0
-        self.B = 0
-        self.LeftThumb = 0
-        self.RightThumb = 0
-        self.Back = 0
-        self.Start = 0
-        self.LeftDPad = 0
-        self.RightDPad = 0
-        self.UpDPad = 0
-        self.DownDPad = 0
-
-        self._monitor_thread = threading.Thread(target=self._monitor_controller, args=())
-        self._monitor_thread.daemon = True
-        self._monitor_thread.start()
-
-
-    def read(self): # return the buttons/triggers that you care about in this methode
-        xL = self.LeftJoystickX
-        xR = self.RightJoystickX
-        yR = self.RightJoystickY
-        return [xL, xR, yR]
-
-
-    def _monitor_controller(self):
-        while True:
-            events = get_gamepad()
-            for event in events:
-                if event.code == 'ABS_Y':
-                    self.LeftJoystickY = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
-                elif event.code == 'ABS_X':
-                    self.LeftJoystickX = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
-                elif event.code == 'ABS_RY':
-                    self.RightJoystickY = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
-                elif event.code == 'ABS_RX':
-                    self.RightJoystickX = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
-                elif event.code == 'ABS_Z':
-                    self.LeftTrigger = event.state / XboxController.MAX_TRIG_VAL # normalize between 0 and 1
-                elif event.code == 'ABS_RZ':
-                    self.RightTrigger = event.state / XboxController.MAX_TRIG_VAL # normalize between 0 and 1
-                elif event.code == 'BTN_TL':
-                    self.LeftBumper = event.state
-                elif event.code == 'BTN_TR':
-                    self.RightBumper = event.state
-                elif event.code == 'BTN_SOUTH':
-                    self.A = event.state
-                elif event.code == 'BTN_NORTH':
-                    self.Y = event.state #previously switched with X
-                elif event.code == 'BTN_WEST':
-                    self.X = event.state #previously switched with Y
-                elif event.code == 'BTN_EAST':
-                    self.B = event.state
-                elif event.code == 'BTN_THUMBL':
-                    self.LeftThumb = event.state
-                elif event.code == 'BTN_THUMBR':
-                    self.RightThumb = event.state
-                elif event.code == 'BTN_SELECT':
-                    self.Back = event.state
-                elif event.code == 'BTN_START':
-                    self.Start = event.state
-                elif event.code == 'BTN_TRIGGER_HAPPY1':
-                    self.LeftDPad = event.state
-                elif event.code == 'BTN_TRIGGER_HAPPY2':
-                    self.RightDPad = event.state
-                elif event.code == 'BTN_TRIGGER_HAPPY3':
-                    self.UpDPad = event.state
-                elif event.code == 'BTN_TRIGGER_HAPPY4':
-                    self.DownDPad = event.state
-
-
-class XboxController2(object):
+class XboxController(threading.Thread):
     MAX_TRIG_VAL = math.pow(2, 8)
     MAX_JOY_VAL = math.pow(2, 15)
 
@@ -242,9 +157,7 @@ class XboxController2(object):
 
 
 
-
-#############################################################################################################################################################################################################
-#############################################################################################################################################################################################################
+#####################################################################################################################################################################################################################################################################
 #############################################################################################################################################################################################################
 
 
@@ -281,8 +194,10 @@ try:
     rID1 = int(input("Ingrese el primer ID de robot a controlar: "))
     rID2 = int(input("Ingrese el segundo ID de robot a controlar: "))
     Vmax= int(input("Ingrese velocidad maxima del robot de 0 a 512: "))
-    joy1 = XboxController()
-    joy2 = XboxController()
+
+    joy1 = XboxController(0)
+    joy2 = XboxController(1)
+    
     while True:
         # Obtener las coordenadas del punto final y el tiempo de llegada deseado
         vels1 = joy1.read()
