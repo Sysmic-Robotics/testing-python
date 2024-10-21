fc827da531f1a16fefc26a0769689c88b4a898d1
Nico-Mansilla <nicolas.mansilla@sansano.usm.cl>
2024-10-21 01:34:27 -0300

    implementado 2 controles y actualziado controller1

--- controllerSerial2.py
+++ controllerSerial2.py
@@ -27,11 +27,9 @@ def cacularVelocidades_step(Vtarget,max_acc,num_steps):
     return velocities
    
 def limitar_a_10_bits(numero):
-    # Limitar el rango del número a 10 bits (incluyendo el bit de signo) (para V_x y V_y)
     return max(-512, min(511, numero))
 
 def limitar_a_12_bits(numero):
-    # Limitar el rango del número a 12 bits (incluyendo el bit de signo) (para V_th)
     return max(-2048, min(2047, numero))
 
 def entero_a_binario(n, longitud):
@@ -39,15 +37,10 @@ def entero_a_binario(n, longitud):
         return '0' * longitud
     signo = '0' if n >= 0 else '1'
     n = abs(n)
-    binario = ''
-    while n > 0:
-        binario = str(n % 2) + binario
-        n //= 2
-    return signo + (binario).zfill(longitud-1)
+    binario = bin(n)[2:]  # Convertir a binario y eliminar el prefijo '0b'
+    return signo + binario.zfill(longitud - 1)
 
-# Función para formar un trozo de 5 bytes con la estructura especificada
 def formar_trozo(id_,dribb,kick, velocidad_x, velocidad_y, velocidad_th):
-    #Se saturan los numeros a 10 bits (+511 -512) y 12 bits (+2047 -2048) respectivamente
     velocidad_x = limitar_a_10_bits(velocidad_x)
     velocidad_y = limitar_a_10_bits(velocidad_y)
     velocidad_th = limitar_a_12_bits(velocidad_th)
@@ -56,24 +49,22 @@ def formar_trozo(id_,dribb,kick, velocidad_x, velocidad_y, velocidad_th):
     b = entero_a_binario(velocidad_y,10)
     c = entero_a_binario(velocidad_th,12)
     
-    # Formar el trozo de 5 bytes
     trozo = bytearray([
-        (((id_<<4)+dribb)<<1)+kick,  # Primer byte: id | dribb | kick
-        int(a[0] + (a[3:]),2),  # segundo byte: signo | LSB Vx
-        int(b[0] + (b[3:]),2),  # Tercer byte: signo | LSB Vy
-        int(c[0] + (c[5:]),2),  # Cuarto byte: signo | LSB Vth
-        int((a[1:3]) + (b[1:3]) + (c[1:5]),2)  # Quinto byte: MSB Vx | MSB Vy |MSB Vth  int(velocidad_x >> 6) | ((velocidad_y >> 6) << 2) | ((velocidad_th >> 4) << 4)
+        (((id_ << 4) + dribb) << 1) + kick,
+        int(a[0] + a[3:], 2),
+        int(b[0] + b[3:], 2),
+        int(c[0] + c[5:], 2),
+        int(a[1:3] + b[1:3] + c[1:5], 2)
     ])
     return trozo
-
-
 
 class XboxController(threading.Thread):
     MAX_TRIG_VAL = math.pow(2, 8)
     MAX_JOY_VAL = math.pow(2, 15)
 
-    def __init__(self):
-
+    def __init__(self, index):
+        threading.Thread.__init__(self)
+        self.index = index
         self.LeftJoystickY = 0
         self.LeftJoystickX = 0
         self.RightJoystickY = 0
@@ -99,57 +90,57 @@ class XboxController(threading.Thread):
         self._monitor_thread.daemon = True
         self._monitor_thread.start()
 
-
     def readInputs(self):
         return [self.LeftJoystickX, self.RightJoystickX, self.RightJoystickY]
 
-
-
     def _monitor_controller(self):
         while True:
-            events = device.gamepads[self.index].read()
-            for event in events:
-                if event.code == 'ABS_Y':
-                    self.LeftJoystickY = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
-                elif event.code == 'ABS_X':
-                    self.LeftJoystickX = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
-                elif event.code == 'ABS_RY':
-                    self.RightJoystickY = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
-                elif event.code == 'ABS_RX':
-                    self.RightJoystickX = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
-                elif event.code == 'ABS_Z':
-                    self.LeftTrigger = event.state / XboxController.MAX_TRIG_VAL # normalize between 0 and 1
-                elif event.code == 'ABS_RZ':
-                    self.RightTrigger = event.state / XboxController.MAX_TRIG_VAL # normalize between 0 and 1
-                elif event.code == 'BTN_TL':
-                    self.LeftBumper = event.state
-                elif event.code == 'BTN_TR':
-                    self.RightBumper = event.state
-                elif event.code == 'BTN_SOUTH':
-                    self.A = event.state
-                elif event.code == 'BTN_NORTH':
-                    self.Y = event.state #previously switched with X
-                elif event.code == 'BTN_WEST':
-                    self.X = event.state #previously switched with Y
-                elif event.code == 'BTN_EAST':
-                    self.B = event.state
-                elif event.code == 'BTN_THUMBL':
-                    self.LeftThumb = event.state
-                elif event.code == 'BTN_THUMBR':
-                    self.RightThumb = event.state
-                elif event.code == 'BTN_SELECT':
-                    self.Back = event.state
-                elif event.code == 'BTN_START':
-                    self.Start = event.state
-                elif event.code == 'BTN_TRIGGER_HAPPY1':
-                    self.LeftDPad = event.state
-                elif event.code == 'BTN_TRIGGER_HAPPY2':
-                    self.RightDPad = event.state
-                elif event.code == 'BTN_TRIGGER_HAPPY3':
-                    self.UpDPad = event.state
-                elif event.code == 'BTN_TRIGGER_HAPPY4':
-                    self.DownDPad = event.state
-
+            if self.index < len(devices.gamepads):
+                events = devices.gamepads[self.index].read()
+                for event in events:
+                    if event.code == 'ABS_Y':
+                        self.LeftJoystickY = event.state / XboxController.MAX_JOY_VAL
+                    elif event.code == 'ABS_X':
+                        self.LeftJoystickX = event.state / XboxController.MAX_JOY_VAL
+                    elif event.code == 'ABS_RY':
+                        self.RightJoystickY = event.state / XboxController.MAX_JOY_VAL
+                    elif event.code == 'ABS_RX':
+                        self.RightJoystickX = event.state / XboxController.MAX_JOY_VAL
+                    elif event.code == 'ABS_Z':
+                        self.LeftTrigger = event.state / XboxController.MAX_TRIG_VAL
+                    elif event.code == 'ABS_RZ':
+                        self.RightTrigger = event.state / XboxController.MAX_TRIG_VAL
+                    elif event.code == 'BTN_TL':
+                        self.LeftBumper = event.state
+                    elif event.code == 'BTN_TR':
+                        self.RightBumper = event.state
+                    elif event.code == 'BTN_SOUTH':
+                        self.A = event.state
+                    elif event.code == 'BTN_NORTH':
+                        self.Y = event.state
+                    elif event.code == 'BTN_WEST':
+                        self.X = event.state
+                    elif event.code == 'BTN_EAST':
+                        self.B = event.state
+                    elif event.code == 'BTN_THUMBL':
+                        self.LeftThumb = event.state
+                    elif event.code == 'BTN_THUMBR':
+                        self.RightThumb = event.state
+                    elif event.code == 'BTN_SELECT':
+                        self.Back = event.state
+                    elif event.code == 'BTN_START':
+                        self.Start = event.state
+                    elif event.code == 'BTN_TRIGGER_HAPPY1':
+                        self.LeftDPad = event.state
+                    elif event.code == 'BTN_TRIGGER_HAPPY2':
+                        self.RightDPad = event.state
+                    elif event.code == 'BTN_TRIGGER_HAPPY3':
+                        self.UpDPad = event.state
+                    elif event.code == 'BTN_TRIGGER_HAPPY4':
+                        self.DownDPad = event.state
+            else:
+                print(f"Gamepad index {self.index} is out of range. Available gamepads: {len(devices.gamepads)}")
+                time.sleep(1)
 
 def aplicar_zona_muerta(valor, zona_muerta):
     if abs(valor) < zona_muerta:
@@ -162,7 +153,7 @@ def limitar_velocidad(vX, vY, Vmax):
         factor = Vmax / magnitud
         vX *= factor
         vY *= factor
-    return vX, vY
+    return int(vX), int(vY)
 
 def detectar_puerto_serial():
     puertos = list(serial.tools.list_ports.comports())
@@ -171,59 +162,72 @@ def detectar_puerto_serial():
             return puerto.device
     raise Exception("No se encontró un puerto serial adecuado.")
 
-#############################################################################################################################################################################################################
-
-
-
-
-# Crear el buffer de 30 bytes
 buffer = bytearray(30)
 
 trozos = [
-    formar_trozo(0b001,0,0,0,0,0),  # Trozo 1
-    formar_trozo(0b010,0,0,0,0,0),   # Trozo 2
-    formar_trozo(0b011,0,0,0,0,0),   # Trozo 3
-    formar_trozo(0b100,0,0,0,0,0),  # Trozo 4
-    formar_trozo(0b101,0,0,0,0,0),  # Trozo 5
-    formar_trozo(0b110,0,0,0,0,0)   # Trozo 6
+    formar_trozo(0b001,0,0,0,0,0),
+    formar_trozo(0b010,0,0,0,0,0),
+    formar_trozo(0b011,0,0,0,0,0),
+    formar_trozo(0b100,0,0,0,0,0),
+    formar_trozo(0b101,0,0,0,0,0),
+    formar_trozo(0b110,0,0,0,0,0)
 ]
-
-# Insertar los trozos en el buffer
 
 for i in range(5):
     buffer[i*5:i*5+5] = trozos[i]
 
-
-
 # Configurar el puerto serial
-puerto_serial = serial.Serial('COM9', 115200, bytesize=8, parity='N', stopbits=1, timeout=1)
-
+#puerto_serial = serial.Serial('COM9', 115200, bytesize=8, parity='N', stopbits=1, timeout=1)
+try:
+    puerto_dispositivo = detectar_puerto_serial()
+    puerto_serial = serial.Serial(puerto_dispositivo, 115200, bytesize=8, parity='N', stopbits=1, timeout=1)
+    print(f"Puerto serial {puerto_dispositivo} abierto correctamente.")
+except serial.SerialException as e:
+    print(f"Error al abrir el puerto serial: {e}")
+except Exception as e:
+    print(f"Error: {e}")
 
 Vmax= 30
-
-zona_muerta = 5  # Define la zona muerta
+zona_muerta = 5
 
 try:
-    rID1 = int(input("Ingrese el primer ID de robot a controlar: "))
-    rID2 = int(input("Ingrese el segundo ID de robot a controlar: "))
-    Vmax= int(input("Ingrese velocidad maxima del robot de 0 a 512: "))
+    while True:
+        try:
+            rID1 = int(input("Ingrese el primer ID de robot a controlar (0-5): "))
+            rID2 = int(input("Ingrese el segundo ID de robot a controlar (0-5): "))
+            if rID1 == rID2:
+                raise ValueError("Los IDs de los robots no pueden ser iguales.")
+            if not (0 <= rID1 <= 5) or not (0 <= rID2 <= 5):
+                raise ValueError("Los IDs deben estar entre 0 y 5.")
+            break
+        except ValueError as e:
+            print(f"Error: {e}. Por favor, intente de nuevo.")
+
+    while True:
+        try:
+            Vmax = int(input("Ingrese velocidad maxima del robot de 0 a 512 (capado a 150): "))
+            if Vmax > 150:
+                Vmax = 150
+            elif Vmax < 0:
+                Vmax = 0
+            break
+        except ValueError:
+            print("Error: La velocidad máxima debe ser un número entero. Por favor, intente de nuevo.")
 
     joy1 = XboxController(0)
     joy2 = XboxController(1)
     
     while True:
-        # Obtener las coordenadas del punto final y el tiempo de llegada deseado
         vels1 = joy1.readInputs()
         vels2 = joy2.readInputs()
 
-        vX_A1 = aplicar_zona_muerta(vels1[2], zona_muerta)
-        vY_A1 = aplicar_zona_muerta(vels1[1], zona_muerta)
-        vTH_A1 = aplicar_zona_muerta(vels1[0], zona_muerta)
+        vX_A1 = vels1[2]
+        vY_A1 = vels1[1]
+        vTH_A1 = vels1[0]
 
-        vX_A2 = aplicar_zona_muerta(vels2[2], zona_muerta)
-        vY_A2 = aplicar_zona_muerta(vels2[1], zona_muerta)
-        vTH_A2 = aplicar_zona_muerta(vels2[0], zona_muerta)
-
+        vX_A2 = vels2[2]
+        vY_A2 = vels2[1]
+        vTH_A2 = vels2[0]
 
         vX1 = round(vX_A1*Vmax)
         vY1 = round(vY_A1*Vmax)
@@ -232,13 +236,21 @@ try:
         vX2 = round(vX_A2*Vmax)
         vY2 = round(vY_A2*Vmax)
         vTH2 = round(vTH_A2*Vmax)
+
+        vX1 = aplicar_zona_muerta(vX1, zona_muerta)
+        vY1 = aplicar_zona_muerta(vY1, zona_muerta)
+        vTH1 = aplicar_zona_muerta(vTH1, zona_muerta)
+
+        vX2 = aplicar_zona_muerta(vX2, zona_muerta)
+        vY2 = aplicar_zona_muerta(vY2, zona_muerta)
+        vTH2 = aplicar_zona_muerta(vTH2, zona_muerta)
+
+
         
-        # Limitar las velocidades para cumplir con Vmax <= sqrt(x^2 + y^2)
         vX1, vY1 = limitar_velocidad(vX1, vY1, Vmax)
         vX2, vY2 = limitar_velocidad(vX2, vY2, Vmax)
 
 
-         # Formar los trozos con las velocidades calculadas
         trozos = []
 
         for i in range(6):
@@ -249,21 +261,19 @@ try:
             else:
                 trozos.append(formar_trozo(0b001,0,0,0,0,0))
 
-        # Insertar los trozos en el buffer
+        print(f"trozos: {trozos}")  # Debugging line
+
         for i in range(6):
             buffer[i*5:i*5+5] = trozos[i]
         
-        print("vX1")
-        print(vX1)
-        print("vX2")
-        print(vX2)
-        
-        puerto_serial.write(buffer)
-        print("Paquete de datos enviado correctamente.")
-        time.sleep(0.05)
-        
-        
+        print(f"vX1: {vX1}, vY1: {vY1}, vTH1: {vTH1}")  # Debugging line
+        print(f"vX2: {vX2}, vY2: {vY2}, vTH2: {vTH2}")
 
+
+        puerto_serial.write(buffer)
+        print("Paquete de datos enviado correctamente.\n")
+        time.sleep(0.01)
+        
 except KeyboardInterrupt:
     print("\nPrograma terminado por el usuario.")
 except serial.SerialException as e:
@@ -271,4 +281,4 @@ except serial.SerialException as e:
 finally:
     # Cerrar el puerto serial
     puerto_serial.close()
-
+    print("Puerto serial cerrado correctamente.")
